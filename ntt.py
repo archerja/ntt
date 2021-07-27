4f1b00628d17782ad8c93a26b0f110de37603a94
Joseph Archer <joseph.allen.archer@gmail.com>
2021-07-27 11:11:11 -0400

    version 0.0.5

--- ntt.py
+++ ntt.py
@@ -6,33 +6,57 @@ import os
 import sys
 import random
 
-version = '0.0.4'
+version = '0.0.5'
 
-# create empty array
+# create empty arrays
 mp3s = []
+patharray = []
 
-# number of seconds
+# number of seconds to play
 end = 5
 
-# set your music paths (with / as end of path)
-mediapath1 = '/media/archerja/My Passport/backup/Music/Compilation/AM Gold 1962 - 1979/'
-mediapath2 = '/media/archerja/My Passport/backup/Music/Compilation/Ultimate Seventies/'
-mediapath3 = '/media/archerja/My Passport/backup/Music/Compilation/Flower Power/'
-mediapath4 = '/media/archerja/My Passport/backup/Music/Compilation/Columbia Country Classics/'
-mediapath5 = '/media/archerja/My Passport/backup/Music/Compilation/101 One Hit Wonders/'
-mediapath6 = '' 
-mediapath7 = '/media/archerja/My Passport/backup/Music/Compilation/Disco Fever/'
+# name your file of directories below
+songfile = 'songs'
 
 # create list of songs
 def makelist(listnum,listpath):
-        for root, dir, files in os.walk(listpath):
-            for name in files:
-                if name[-4:].lower() == '.mp3':
-                    path = os.path.join(root,name)
-                    mp3s.append([listnum,path])
+        if listnum < 10:
+            for root, dir, files in os.walk(listpath):
+                for name in files:
+                    if name[-4:].lower() == '.mp3':
+                        path = os.path.join(root,name)
+                        mp3s.append([listnum,path])
         if pickcount(mp3s,listnum) > 0:
             print('(' + str(pickcount(mp3s,listnum)) + ' of ' + str(len(mp3s)) + ')' + listpath)
+        else:
+            print('no music loaded')
         return mp3s
+
+def getlocations():
+        pathnum = 0
+        if str(os.path.exists(songfile)) == 'True':
+            with open(songfile) as f:
+                for line in f:
+                    if line[0] == '#' or line[0] == '\n':
+                        continue
+                    else:
+                        line = line.strip('\n')
+                        if line[-1:] == '/':
+                            pass
+                        else:
+                            line = line + '/'
+                        pathnum = pathnum + 1
+                        patharray.append([pathnum,line])
+            print('Loading music from: ')
+            for k in range(0,len(patharray)):
+                for q in range(0,len(patharray[k])):
+                    continue
+                makelist(patharray[k][0], patharray[k][1])
+        else:
+            print('* error: could not find file [' + songfile + ']') 
+            print('* error: check current directory')
+            sys.exit(0)
+        return
 
 def pickcount(songs,pick):
         num = [row[0] for row in songs]
@@ -86,6 +110,7 @@ def exitbanner():
         print(' ')
 
 def help():
+        print(' ')
         print('Help: ')
         print('Help: ')
         print('Help: ')
@@ -100,25 +125,20 @@ def help():
         print('Help: ')
         print('Help: ')
         print('Help: ')
+        print('Help: ')
+        print(' ')
 
 def loadgame():
         clearscreen()
-        print('Loading music from: ')
-        print(' ')
-        makelist(1,mediapath1)
-        makelist(2,mediapath2)
-        makelist(3,mediapath3)
-        makelist(4,mediapath4)
-        makelist(5,mediapath5)
-        makelist(6,mediapath6)
-        makelist(7,mediapath7)
+        getlocations()
         if len(mp3s) == 0:
-            print('????? No songs loaded in game...')
-            print('????? Check folder locations...')
-            exitbanner()
+            print('* error: No songs loaded in game...')
+            print('* error: Check file for correct directory paths...')
             sys.exit(0)
         print(' ')
         print('shuffling ' + str(len(mp3s)) + ' total songs...')
+        random.shuffle(mp3s)
+        random.shuffle(mp3s)
         random.shuffle(mp3s)
         print(' ')
         input('...press enter to begin the game!')
@@ -135,21 +155,24 @@ def main(mp3s):
                         print('-------------------------------------')
                         print('Select which song list:')
                         if pickcount(mp3s,1) > 0:
-                            print('  (a) ' + mediapath1.split('/')[-2])
+                            print('  (a) ' + patharray[0][1].split('/')[-2])
                         if pickcount(mp3s,2) > 0:
-                            print('  (b) ' + mediapath2.split('/')[-2])
+                            print('  (b) ' + patharray[1][1].split('/')[-2])
                         if pickcount(mp3s,3) > 0:
-                            print('  (c) ' + mediapath3.split('/')[-2])
+                            print('  (c) ' + patharray[2][1].split('/')[-2])
                         if pickcount(mp3s,4) > 0:
-                            print('  (d) ' + mediapath4.split('/')[-2])
+                            print('  (d) ' + patharray[3][1].split('/')[-2])
                         if pickcount(mp3s,5) > 0:
-                            print('  (e) ' + mediapath5.split('/')[-2])
+                            print('  (e) ' + patharray[4][1].split('/')[-2])
                         if pickcount(mp3s,6) > 0:
-                            print('  (f) ' + mediapath6.split('/')[-2])
+                            print('  (f) ' + patharray[5][1].split('/')[-2])
                         if pickcount(mp3s,7) > 0:
-                            print('  (g) ' + mediapath7.split('/')[-2])
+                            print('  (g) ' + patharray[6][1].split('/')[-2])
+                        if pickcount(mp3s,8) > 0:
+                            print('  (h) ' + patharray[7][1].split('/')[-2])
+                        if pickcount(mp3s,9) > 0:
+                            print('  (i) ' + patharray[8][1].split('/')[-2])
                         response = input('Pick letter, (?)help, or (q)uit, then press enter: ')
-                        #response = input('[a/b/c/d/e/f/g/?/q] enter: ')
                         if 'q' in response:
                             exitbanner()
                             sys.exit(0)
@@ -194,6 +217,18 @@ def main(mp3s):
                                 continue
                         elif 'g' in response:
                             picklist = 7
+                            if pickcount(mp3s,picklist) > 0:
+                                status = False
+                            else:
+                                continue
+                        elif 'h' in response:
+                            picklist = 8
+                            if pickcount(mp3s,picklist) > 0:
+                                status = False
+                            else:
+                                continue
+                        elif 'i' in response:
+                            picklist = 9
                             if pickcount(mp3s,picklist) > 0:
                                 status = False
                             else:
